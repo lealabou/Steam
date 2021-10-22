7525850449d3c11878682c46fa235f7fb794d3ea
lea labourdere <lealabourdere@gmail.com>
2021-10-22 14:38:02 +0200

    Initiale commit

---
+++ src/DataFixtures/CatalogueFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Catalogues;
+
+class CatalogueFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for($i = 1; $i<10;$i++)
+        {
+            $catalogues = new catalogues();
+            $catalogues ->setTitre("Nom du Jeu n°$i")
+                        ->setDescription("<p>Description du Jeu n°$i</p>")
+                        ->setImage("http://placehold.it/350x150")
+                        ->setCategorie("<p>Nom de la Categorie</p>")
+                        ->setDate(1);
+            $manager->persist($catalogues);
+
+        }
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Catalogues.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CataloguesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CataloguesRepository::class)
+ */
+class Catalogues
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getDate(): ?int
+    {
+        return $this->Date;
+    }
+
+    public function setDate(int $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+}
