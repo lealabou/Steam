b204a486b2f923e8b786bca66157e4e3e2bb8e36
lea labourdere <lealabourdere@gmail.com>
2021-10-22 15:34:51 +0200

    22/10/21 15:34

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,15 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Catalogues;
 class AccueilController extends AbstractController
 {
 
     //#[Route('/accueil', name: 'accueil')]
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->GetRepository(Catalogue::class);
+
+        $catalogues = $repo->FindAll();
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'articles' => $catalogues
         ]);
+    }
+
+    /**
+     * @Route("/", name= "home")
+     */
+
+    public function home()
+    {
+        return $this->render('accueil/home.html.twig');
     }
 }
