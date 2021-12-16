93a7996294dbdbd73002fd5540a5d148edb8bef1
Justine REVOIL <justine.revoil@etu.uca.fr>
2021-12-16 09:15:38 +0100

    test pour l'edit profile

--- src/DataFixtures/CatalogueFixtures.php
+++ src/DataFixtures/CatalogueFixtures.php
@@ -6,12 +6,13 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Catalogues;
 use App\Entity\Comment;
+use Zenstruck\Foundry\Factory;
 
 class CatalogueFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Faker\Factory::create(FR_fr);
+#        $faker = Faker\Factory::create(fr_FR);
 
         for($i = 1; $i<10;$i++)
         {
@@ -22,21 +23,21 @@ class CatalogueFixtures extends Fixture
                         ->setCategorie("<p>Nom de la Categorie</p>")
                         ->setDate(1)
                         ->setPrix(1)
-                        ->setLienDuJeu("<p>Lien du jeu n°i</p>");
+                        ->setTelechargement("<p>Lien du jeu n°i</p>");
             $manager->persist($catalogues);
 
-            for($j = 1; $j <= mt_rand(4,10); $j++){
-                $comment = new \Comment();
-                
-                $content = '<p>' . join($faker->paragraphs(2),
-                '</p><p>') . '</p>';
+#            for($j = 1; $j <= mt_rand(4,10); $j++){
+#                $comment = new \Comment();
+#                
+#                $content = '<p>' . join($faker->paragraphs(2),
+#                '</p><p>') . '</p>';
 
-                $comment->setAuteur($faker->name)
-                        ->setContenue($content)
-                        ->setDate($faker->dateTimeBetween('6 months'))
-                        ->setCatelogues($catalogues);
-                $mangager->persist($comment);
-            }
+#                $comment->setAuteur($faker->name)
+#                        ->setContenue($content)
+#                        ->setDate($faker->dateTimeBetween('6 months'))
+#                        ->setCatelogues($catalogues);
+#                $mangager->persist($comment);
+#            }
         }
         $manager->flush();
     }
